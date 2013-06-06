403f337f9f450d05f08baba850cd2b0367e88e59
Andrew Clarkson <andrewclarkson@live.com>
2013-06-05 20:30:17 -0500

    documenting private methods

--- ftw.js
+++ ftw.js
@@ -16,14 +16,16 @@ var util = require('util');
 
 /** 
  * Creates a File Tree Walker
+ * 
  * @constructor
  * @param {object} options - a map of options
  * @augments EventEmitter
  */
+
 var Walker = function(options) {
 
     /**
-    * a map of types of entries we may encounter in a directory paired with 
+    * A map of types of entries we may encounter in a directory paired with 
     * their corresponding stat functions 
     * @see {@link http://nodejs.org/api/fs.html#fs_class_fs_stats|Node.js API}
     */
@@ -37,12 +39,13 @@ var Walker = function(options) {
     };
 
     /**
-     * a running array of the directories we have already walked to avoid 
-     * duplicates
+     * A array of the directories we have already walked to avoid duplicates
      */
     this.walked = [];
 
-    // if depth is not set or it's < 0 set depth to the default: 10
+    /**
+     * The maximum recursion depth
+     */
     this.depth = options && options.depth >= 0 ? options.depth : 10;
 };
 
@@ -50,15 +53,17 @@ util.inherits(Walker, events.EventEmitter);
 
 /**
  * Walks a directory firing events on files and directories
+ * 
  * @param {String|String[]} directories - a directory path or an array of paths
- * @fires Walker#block
- * @fires Walker#character
- * @fires Walker#directory
- * @fires Walker#done
- * @fires Walker#error
- * @fires Walker#fifo
- * @fires Walker#file
- * @fires Walker#socket
+ * 
+ * @fires module:FTW~Walker#block
+ * @fires module:FTW~Walker#character
+ * @fires module:FTW~Walker#directory
+ * @fires module:FTW~Walker#done
+ * @fires module:FTW~Walker#error
+ * @fires module:FTW~Walker#fifo
+ * @fires module:FTW~Walker#file
+ * @fires module:FTW~Walker#socket
  */
 
 Walker.prototype.walk = function(directories) {
@@ -66,17 +71,17 @@ Walker.prototype.walk = function(directories) {
     /** @this Walker */
     var self = this;
 
-    /**
-     * if the input is only one directory (a string) convert it into an array
+    /*
+     * If the input is only one directory (a string) convert it into an array
      * with one element
      */
     if(typeof directories === 'string') {
         directories = Array(directories);
     }
 
-    /**
-     * walk each directory with a callback that emits the done event 
-     * @see{Walker#done}
+    /*
+     * Walk each directory with a callback that emits the done event 
+     * @see{module:FTW~Walker#done}
      */
     self._walkEach(directories, function() {
         self.emit('done');
@@ -84,34 +89,32 @@ Walker.prototype.walk = function(directories) {
 };
 
 /**
- * Calls _walk on each directory @see{@link _walk}
+ * Calls {@link module:FTW~Walker#_walk|_walk} on each directory.
+ * 
  * @private
  * @param {String[]} directories - the directories to walk
- * @param {walkedEach} callback - the callback to call when we have walked each
+ * @param {Function} callback - the callback to call when we have walked each
  * directory
- */
-
-/**
- * Returns from a level of recursion / jump up a level
- * @callback walkedEach
  */
 
 Walker.prototype._walkEach = function(directories, callback) {
    
     /** @this Walker */
     var self = this;
+    
+    /* if we have reached the maximum recursion depth go no further */
+    if(self.depth < 0) {
+        return callback();
+    }
+    
+    /* decrement the maximum depth so that eventually we will hit it */
+    self.depth -= 1;
 
-    /** 
+    /* 
      * because the functions are asynchronous we need to set up a pending 
      * callbacks counter
      */
     var pending = 0;
-    
-    if(self.depth < 0) {
-        return callback();
-    }
-
-    self.depth -= 1;
 
     directories.forEach(function(directory) {
         self._walk(directory, function() {
@@ -127,7 +130,7 @@ Walker.prototype._walkEach = function(directories, callback) {
             }
         });
 
-        /** increment the pending callbacks; we just created one with _walk */
+        /** add a pending callback */
         pending += 1;
     });
 };
@@ -137,13 +140,8 @@ Walker.prototype._walkEach = function(directories, callback) {
  * Recursively walks a directory
  * @private
  * @param {String} directory - the directory to walk
- * @param {walked} callback - the callback to call when we have walked the
+ * @param {Function} callback - the callback to call when we have walked the
  * directory
- */
-
-/**
- * Return from walking a directory
- * @callback walked
  */
 
 Walker.prototype._walk = function(directory, callback) {
@@ -151,27 +149,39 @@ Walker.prototype._walk = function(directory, callback) {
     /** @this Walker */
     var self = this;
 
-    /* if we have already walked this directory */
+    /* if we have already walked this directory skip the directory */
     if(self.walked.indexOf(directory) !== -1) {
-        return callback;
+        return callback();
     }
 
-    self.walked.push(directory);
-    
     fs.readdir(directory, function(error, results) {
         
         if(error){
             self.emit('error', error);
-            return callback(); // go no further
+            return callback();
         }
 
-        /* now we can be sure the directory exists */
+        /* now that we can be sure the directory exists, emit the event */
         self.emit('directory', directory);
 
         self._statEach(results, directory, callback);
 
     });
+
+    /* add this directory to the array of directories we have walked */
+    self.walked.push(directory);
 };
+
+/**
+ * Creates the {@link http://nodejs.org/api/fs.html#fs_class_fs_stats|Stat}
+ * object for each result
+ * @private
+ * @param {String[]} results - an array of file or directory paths to pass to 
+ * fs.stat
+ * @param {String} directory - the directory currently being walked
+ * @param {Function} callback - the callback to call when we have stat'd each
+ * result
+ */
 
 Walker.prototype._statEach = function(results, directory, callback) {
    
@@ -182,22 +192,16 @@ Walker.prototype._statEach = function(results, directory, callback) {
      * because this is a "breadth first search" we queue the 
      * directories until we have found all the non-recursive stuff
      */
-
-    var pending = 0;
-
     var queue = [];
     
-    /* since this is async we need a counter to count the callbacks */
-    var counter = results.length;
+    /* since this is async we need a counter to count the pending callbacks */
+    var pending = 0;
 
     results.forEach(function(result) {
-        
-        pending += 1;
-        
         result = path.join(directory, result);
-
         fs.stat(result, function(error, stats) {
             
+            /* remove a pending callback */
             pending -= 1;
             
             if(error) { 
@@ -211,10 +215,10 @@ Walker.prototype._statEach = function(results, directory, callback) {
                 // takes the map of entry types and checks each
                 self._emitTypes(result, stats);
             }
-            // if this is the last entry and we haven't hit our 
-            // recursion limit, walk the queue
+            /* if this is the last entry, walk the queue */
             if(pending === 0) {
 
+                /* if the queue is empty, bubble up callback stack */
                 if(queue.length === 0) {
                     return callback();
                 }
@@ -222,16 +226,32 @@ Walker.prototype._statEach = function(results, directory, callback) {
                 self._walkEach(queue, callback);
             }
         });
+
+        /** add a pending callback */
+        pending += 1;
     });
 };
+
+/**
+ * Emits events based on the type of the "file" that was fs.stat'd
+ * @private
+ * @param {String} result - the path to the file
+ * @param {Object} stats - the stat object returned from fs.stat
+ * @see {@link http://nodejs.org/api/fs.html#fs_class_fs_stats|Node.js API}
+ */
 
 Walker.prototype._emitTypes = function(result, stats) {
     
     /** @this Walker */
     var self = this;
     
+    /** 
+     * for each key of the types object test if the result is that type using
+     * the corresponding stat.is____() function, if it is emit the event for 
+     * that type
+     */
+
     Object.keys(self.types).forEach(function(key) {
-        // the isType() stat function
         var test = self.types[key];
         if(stats[test]()) {
             self.emit(key, result);
@@ -239,24 +259,53 @@ Walker.prototype._emitTypes = function(result, stats) {
     });
 };
 
+module.exports = Walker;
+
 /* Event documentation */
 
 /**
- * Block device event.
- * @event Walker#block
- * @type {String}
+ * Block device event. Fired upon finding a block device file.
+ * @event module:FTW~Walker#block
+ * @param {String} path - a path to the block device
  */
 
 /**
- * Character device event.
- * @event Walker#character
- * @type {String}
+ * Character device event. Fired upon finding a character device file.
+ * @event module:FTW~Walker#character
+ * @param {String} path - a path to the character device
  */
 
 /**
- * Directory event
- * @event Walker#dir
- * @type {String}
+ * Directory event. Fired upon finding a directory.
+ * @event module:FTW~Walker#character
+ * @param {String} path - a path to the directory
  */
 
-module.exports = Walker;
+/**
+ * Done event. Fired upon finishing walking all the directories.
+ * @event module:FTW~Walker#done
+ */
+
+/**
+ * Error event. Fired upon an error while walking.
+ * @event module:FTW~Walker#error
+ * @param {Object} error - the error object
+ */
+
+/**
+ * FIFO event. Fired upon FIFO pipe.
+ * @event module:FTW~Walker#fifo
+ * @param {String} path - a path to the pipe
+ */
+
+/**
+ * File event. Fired upon finding a standard file.
+ * @event module:FTW~Walker#file
+ * @param {String} path - a path to the file
+ */
+
+/**
+ * Socket event. Fired upon finding a socket.
+ * @event module:FTW~Walker#socket
+ * @param {String} path - a path to the socket
+ */
